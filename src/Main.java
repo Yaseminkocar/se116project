45f7a8f6e4f5566d394e60b73ba5c057f46b5c2b
Betül Sinem Çetiner <betul.sinem@std.ieu.edu.tr>
2024-05-01 20:30:27 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("asdasd");
-        System.out.println("wduehkcekcjnöe");
-        System.out.println("emiray yazdi ");
-        System.out.println("emiray  yasemin sinem meltem");
-        System.out.println("sinemin comiti ");
-        System.out.println("oldu");
+
     }
 }
