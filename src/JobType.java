48c60bb2a7da813742366731aaaa862c7829e403
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2024-05-05 12:31:31 +0300

    some changes

--- src/JobType.java
+++ src/JobType.java
@@ -3,6 +3,31 @@ import java.util.ArrayList;
 public class JobType {
     private int jobTypeId;
     private String type;
+
+    public int getJobTypeId() {
+        return jobTypeId;
+    }
+
+    public void setJobTypeId(int jobTypeId) {
+        this.jobTypeId = jobTypeId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Task> getTaskArrayList() {
+        return taskArrayList;
+    }
+
+    public void setTaskArrayList(ArrayList<Task> taskArrayList) {
+        this.taskArrayList = taskArrayList;
+    }
+
     private ArrayList<Task> taskArrayList;
 
     public JobType(int jobTypeId, String type, ArrayList<Task> taskArrayList) {
