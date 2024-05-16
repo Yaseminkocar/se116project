cec8a1b13c208c3b45056e67937b978ee575556e
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2024-05-16 21:52:40 +0300

    reading method

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            String input = readTaskFromFile("InputFile.txt");
+            String input = TaskReading("InputFile.txt");
             String result = modifyTaskFormat(input);
             System.out.println("Modified task statement: " + result);
         } catch (IOException e) {
@@ -14,4 +14,17 @@ public class Main {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+     public static String TaskReading(String Inputfile) throws IOException {
+            StringBuilder stringBuilder = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new FileReader(Inputfile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+            }
+            return stringBuilder.toString();
+        }
+
+
 }
