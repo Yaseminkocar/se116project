162ca7b5ce9303b368f667e191db1ca4d9478793
EmirayDurmaz <emiray09@hotmail.com>
2024-05-16 22:03:34 +0300

    job changes

--- src/Job.java
+++ src/Job.java
@@ -3,15 +3,35 @@ import java.util.ArrayList;
 public class Job {
 
     private JobType jobType; //must start with a letter
-    private String jobID; //must start with a letter
+    private String jobID;
+    private int startTime;
+    private int duration;//must start with a letter
 
     private int jobDuration;
     private ArrayList<Task> taskArrayList = new ArrayList<>();
 
-    public Job(JobType jobType, String jobID, int jobDuration){
+    public Job(JobType jobType, String jobID, int jobDuration,int starTime, int duration){
         this.jobType=jobType;
         this.jobID=jobID;
-        this.jobDuration=jobDuration; // son süre ve ilk süre yok başlama bitiş gibi
+        this.jobDuration=jobDuration;
+       this.startTime=starTime;
+       this.duration=duration;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 
     public void setJobType(JobType jobType){this.jobType=jobType;}
