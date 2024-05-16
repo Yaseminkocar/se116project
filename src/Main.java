7c4e310e7d350a5f767a8507cc5d58861b5abf46
Betül Sinem Çetiner <betul.sinem@std.ieu.edu.tr>
2024-05-16 21:47:47 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            String input = readTaskFromFile("InputFile.txt");
+            String result = modifyTaskFormat(input);
+            System.out.println("Modified task statement: " + result);
+        } catch (IOException e) {
+            System.out.println("Invalid tasktypeID " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
