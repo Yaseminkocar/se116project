eda6a3a0b26c599f85e8574d0cd7761ab78861c7
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2024-05-17 13:15:36 +0300

    commit

---
+++ src/WorkFlowParser.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+public class WorkFlowParser {
+    public static void main(String[] args) {
+        String filePath = "WrongFile.txt";
+
+        try {
+            parseWorkflowFile(filePath);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
