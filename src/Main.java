7e118a06281370aac702c5e2c2f3e8efb84af8ae
meltemdemir <meltemd127@gmail.com>
2024-05-17 12:16:06 +0300

    Arraylist additions

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
-            String input = TaskReading("InputFile.txt");
+            String input = TaskReading("WrongFile.txt");
             String result = Rename(input);
             System.out.println("Modified task statement: " + result);
         } catch (IOException e) {
@@ -15,7 +15,7 @@ public class Main {
 
 
 
-        String filePath = "InputFile.txt"; // Buraya dosya yolunu yazın
+        String filePath = "WrongFile.txt"; // Buraya dosya yolunu yazın
 
         try (BufferedReader bufferreader = new BufferedReader(new FileReader(filePath))) {
             String line;
@@ -34,9 +34,9 @@ public class Main {
 
     }
 
-     public static String TaskReading(String Inputfile) throws IOException {
+     public static String TaskReading(String WrongFile) throws IOException {
          StringBuilder stringBuilder = new StringBuilder();
-         try (BufferedReader reader = new BufferedReader(new FileReader(Inputfile))) {
+         try (BufferedReader reader = new BufferedReader(new FileReader(WrongFile))) {
              String line;
              while ((line = reader.readLine()) != null) {
                  stringBuilder.append(line);
@@ -47,7 +47,7 @@ public class Main {
 
     public static String Rename(String input) {
         if (Character.isDigit(input.charAt(0))) {
-            throw new IllegalArgumentException("Task statement can not start with a number !.");
+            throw new IllegalArgumentException("Task statement can not start with a number!");
         }
 
         return input;
@@ -55,6 +55,8 @@ public class Main {
 
 
     
+
+
 
 
 }
